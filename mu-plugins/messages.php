838860bf2185d65bbe310e7bb59d1e6d066458c1
John Cottrell <jgc@users.noreply.github.com>
2016-06-28 14:12:57 +0100

    Create messages.php

---
+++ mu-plugins/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+//https://wycks.wordpress.com/2013/12/05/how-to-remove-error-notices-using-wordpresss-wp_debug/
+
+/*
+error_reporting(E_ALL &  ~( E_NOTICE | E_USER_NOTICE | E_STRICT | 
+E_DEPRECATED | E_USER_DEPRECATED | E_WARNING | E_CORE_WARNING | 
+E_USER_WARNING | E_COMPILE_WARNING | E_PARSE ));
+*/
